03d965448a7f537bece380742ddf87544c7eda74
ermek <ermaaskarbekov@gmail.com>
2022-11-05 18:53:44 +0600

    Экспортировал в App.tsx все компоненты(Меню, Заказы(добавление и удаление), Счетчик заказанных товаров. Создал папку assets в котором сохранил иконки в формате png.

--- src/Components/Add-order/AddOrder.tsx
+++ src/Components/Add-order/AddOrder.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MenuItems from "./Menu-items";
 
-const AddOrder = () => {
+
+interface AddOrderProps {
+    orderMenu: {name:string, price:number, img: string}[];
+    addOrder: (name:string)=> void;
+}
+
+const AddOrder: React.FC<AddOrderProps> = ({orderMenu, addOrder}) => {
     return (
-        <div>
-
+        <div className="card d-flex p-4" style={{width: "600px"}}>
+            <h2>Add items:</h2>
+            <div className="d-flex flex-wrap justify-content-evenly">
+                {orderMenu.map((order:any, index:any) => {
+                    return (
+                        <MenuItems key={index} order={order} addOrder={addOrder}/>
+                    )
+                })}
+            </div>
         </div>
     );
 };
